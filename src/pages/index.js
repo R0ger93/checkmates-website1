f04a43a9854729c8869550b1b4bebaf2c31d9baa
R0ger93 <rogermcfarland93@gmail.com>
2025-05-24 14:44:25 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 export default function HomePage() {
   return (
     <div
-      className="w-screen min-h-screen bg-cover bg-center text-white font-sans relative"
+      className="w-screen h-screen bg-cover bg-center bg-no-repeat"
         style={{
          backgroundImage: "url('/thumbnail1.jpg')"
          backgroundSize: "cover",
