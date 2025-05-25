8691905bfb48aed3174ab16138cd78a533908be9
R0ger93 <rogermcfarland93@gmail.com>
2025-05-25 05:05:33 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ export default function HomePage() {
     <section
       className="w-screen h-screen bg-cover bg-center bg-no-repeat relative"
       style={{
-        backgroundImage: "url('/thumbnail1.jpg')", // <-- Make sure this file is uploaded in /public
-      }}
+        backgroundImage: "url('/thumbnail1.jpg')",}}
     >
-      {/* Optional dark overlay */}
+      
       <div className="absolute inset-0 bg-black opacity-60"></div>
     </section>
   );
