00ef091588f6b44ce2557eea8b0619be059f4c39
R0ger93 <rogermcfarland93@gmail.com>
2025-05-25 05:08:18 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 export default function HomePage() {
   return (
     <section
-      className="w-screen h-screen bg-cover bg-center bg-no-repeat relative"
+      className="w-screen h-screen bg-cover bg-center bg-no-repeat relative flex justify-center items-center"
       style={{
-        backgroundImage: "url('/thumbnail1.jpg')",}}
+        backgroundImage: "url('/thumbnail1.jpg')", // Make sure this file is in /public
+      }}
     >
-      
-      <div className="absolute inset-0 bg-black opacity-60"></div>
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black opacity-60 z-0"></div>
+
+      {/* Logo centered */}
+      <div className="relative z-10">
+        <img
+          src="/logo.png" // Make sure this file is in /public
+          alt="Checkmates Logo"
+          className="w-64 h-auto mx-auto rounded-xl shadow-xl"
+        />
+      </div>
     </section>
   );
 }
