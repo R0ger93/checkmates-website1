50f415a7eb8a055ba5da6bfc8f9c008b6ea0feb2
R0ger93 <rogermcfarland93@gmail.com>
2025-05-27 12:34:30 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,10 @@
-
+<div
+  className="w-screen min-h-screen bg-cover bg-center bg-no-repeat"
+  style={{
+    backgroundImage: "url('/checkmateblack.png')",
+    backgroundSize: "contain", // this keeps it looking clean
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundColor: "#000", // fills the black around it for seamless look
+  }}
+>
