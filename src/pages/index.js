a2e643df1fb55fd74f7a4357c325b2a9d4fa3fa4
R0ger93 <rogermcfarland93@gmail.com>
2025-05-27 13:05:24 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
 export default function HomePage() {
   return (
     <div
-      className="w-screen h-screen bg-no-repeat bg-center bg-cover"
+      className="w-screen h-screen bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: "url('/checkmateblack.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: "#000",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: '#000'
       }}
     >
-{}
+      {/* Content goes here if needed */}
     </div>
   );
 }
